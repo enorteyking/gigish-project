64b084e8de85dc2e1a7e43723b9d9ebcf8a7d8a0
Emmanuel Nortey-King <nkingemma@.gmail.com>
2025-04-28 07:41:41 +0000

    updates for mvp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         </ul>
         <div className="space-x-4">
           <Link to={'/login'} className="px-4 py-2 border border-indigo-600 text-indigo-600 rounded-full hover:bg-indigo-50 transition">Log in</Link>
-          <Link to={'/sigup'} className="px-4 py-2 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition">Sign Up</Link>
+          <Link to={'/signup'} className="px-4 py-2 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition">Sign Up</Link>
         </div>
       </nav>
     </>
